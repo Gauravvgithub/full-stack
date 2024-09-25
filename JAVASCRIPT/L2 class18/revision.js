b568247486e78968e1309662f14d3c03e217fa9f
Gaurav <gauravraj7011@gmail.com>
2024-09-25 11:17:16 +0530

    revision

---
+++ JAVASCRIPT/L2 class18/revision.js
@@ -0,0 +1,20 @@
+
+
+// trim
+let a="      gaurav      ";
+console.log(a);
+console.log(a.trim());
+
+// concat
+
+let b="    gaurav    ";
+console.log(b.concat());
+
+let c=["gaurav"];
+console.log(c);
+
+
+// proto
+
+let str="hello world";
+console.log(str.__proto__);

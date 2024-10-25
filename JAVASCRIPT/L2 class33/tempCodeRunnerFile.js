d9f660428327a0454a7ad457f008cabc0575bc2b
Gaurav <gauravraj7011@gmail.com>
2024-10-25 23:57:57 +0530

    code push

--- JAVASCRIPT/L2 class33/tempCodeRunnerFile.js
+++ JAVASCRIPT/L2 class33/tempCodeRunnerFile.js
@@ -1,1 +1,14 @@
+import axios from 'axios';
 
+
+async function getdata(url){
+    console.log("vikas")
+    let response = await axios.get(url);
+
+    console.log(response.data)
+
+    console.log("chaudhary")
+
+}
+
+getdata( 'https://randomuser.me/api/')

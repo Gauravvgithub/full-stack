978a518cf4216db18dde59cb441eeffecffc94a8
Gauravvgithub <gauravraj7011@gmail.com>
2025-01-21 01:33:59 +0530

    ecommerce project via react js and react contextAPI , forentend completed

--- REACT/ecommerce-react-project/frontend/src/App.jsx
+++ REACT/ecommerce-react-project/frontend/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
+import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kid_banner from './Components/Assets/banner_kids.png'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Shop/>}/>
+      <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
+      <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
+      <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>}/>
+      <Route path='product' element={<Product/>}> 
+        <Route path=':productId' element={<Product/>}/>
+      </Route>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/login' element={<LoginSignup/>}/>
+    </Routes>
+    <Footer/>
+    </BrowserRouter>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ REACT/ecommerce-react-project/frontend/src/Pages/LoginSignup.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './CSS/LoginSignup.css'
+
+const LoginSignup = () => {
+  return (
+    <div className='loginSignup'>
+        <div className="loginSignup-container">
+            <h1>Sign Up</h1>
+            <div className="loginSignup-fields">
+                <input type="text" placeholder='Your Name'/>
+                <input type="email" placeholder='Email Address' />
+                <input type="password" placeholder='Password' />
+            </div>
+            <button>Continue</button>
+            <p className='loginSignup-login'>Already have an account? <span>Login here</span> </p>
+            <div className="loginSignup-agree">
+                <input type="checkbox" name='' id='' />
+                <p>By continuing, i agree to the terms of use & privacy policy.</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default LoginSignup;
